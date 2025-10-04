2f841a1c7dcd2c2fe096d21fa79bcb9ad147cc9a
Ubuntu <ubuntu@vps-6b3cdbec.vps.ovh.ca>
2025-10-04 05:30:50 +0000

    bridge service + env locks

---
+++ utils/mt_5_bridge.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import time, sys
+print("MT5 bridge placeholder started; replace me with your real bridge logic.", flush=True)
+try:
+    while True:
+        time.sleep(60)
+except KeyboardInterrupt:
+    sys.exit(0)
